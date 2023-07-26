0637f38b6ccd0ff512a1e189fe4d92b4a83fb10e
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-07-25 19:39:29 -0600

    Update xmlreader.py

--- xmlreader.py
+++ xmlreader.py
@@ -545,6 +545,7 @@ def main():
     uploaded_txt_files = st.file_uploader("Carga los archivos txt que contienen la metadata del SAT", type=["txt"], accept_multiple_files=True)
     rfc_filtro = st.text_input('Ingresa el RFC de la sociedad que deseas hacer el análisis:', value='', key='rfc_filtro')
     metodos_pago = pd.read_csv("mp.csv")
+    prov69B = pd.read_csv("Listado_Completo_69-B.csv")
     
     st.write("Selecciona la casilla Procesar 👇")
     if st.checkbox("Procesar"):
@@ -822,6 +823,7 @@ def main():
 
             
             st.dataframe(metodos_pago)
+            st.dataframe(prov69B)
 
 
             if st.session_state.rfc_filtro:
