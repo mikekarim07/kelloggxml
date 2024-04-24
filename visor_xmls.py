12eeff2dbf442228b0afae6efaee8a0e11f5805d
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-04-24 11:40:40 -0600

    Update visor_xmls.py

--- visor_xmls.py
+++ visor_xmls.py
@@ -561,8 +561,8 @@ if 'rfc_filtro' not in st.session_state:
 
 def main():
     
-    # st.image("https://www.kellanovaus.com/content/dam/NorthAmerica/kellanova-us/images/logo.svg", width=150)
-    st.image("https://www.sat.gob.mx/satys/img/logo3.png", width=300)
+    st.image("https://www.kellanovaus.com/content/dam/NorthAmerica/kellanova-us/images/logo.svg", width=150)
+    # st.image("https://www.sat.gob.mx/satys/img/logo3.png", width=300)
     st.title("Plataforma Web para extraer datos de los CFDIs")
     with st.sidebar.expander("Instrucciones"):
         st.subheader("Instrucciones")
